26758cf6b975f13fedb06d2132ba7e3789494b81
noranau <alyaananditanaura@gmail.com>
2025-05-20 07:53:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as 
 
-st.title("Hello and welcome to the Fastest Bear's World") 
+st.title("Hello and welcome to the Fastest Bear's World.") 
 st.write(
     "Yap about Oliver Bearman with Nauranora."
 )
