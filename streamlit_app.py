958e4af909671e48dda6674366ad36369ab1f693
noranau <alyaananditanaura@gmail.com>
2025-05-27 07:54:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ st.write(
 st.image("IMG_20250517_151548_895.jpg", width=200)
 
 
-
 st.title("The Simple App") 
 st.header("Aplikasi Mengecek Nilai Genap/Ganjil") 
 angka = st.number_input("Tulis sebuah Angka:", value=0, step=1
