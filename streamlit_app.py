67cae96cc9141c68f3c5ae8161d73be24878361d
noranau <alyaananditanaura@gmail.com>
2025-05-27 07:49:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,14 @@ st.write(
     "Yap about Oliver Bearman with Nauranora."
 )
 st.image("IMG_20250517_151548_895.jpg", width=200)
+
+
+
+st.title("The Simple App") 
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil") 
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1
+
+if (angka % 2) == 0:
+  st.write(f"{angka} adalah Bilangan Genap") 
+else: 
+  st.write(f"{angka} adalah Bilangan Ganjil")              
